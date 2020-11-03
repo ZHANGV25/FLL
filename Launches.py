e2e2bd1e65227e923d029160df87f66f1ce0dbb6
ZHANGV25 <56682630+ZHANGV25@users.noreply.github.com>
2020-11-03 13:11:28 -0800

    deleted last years stufff

--- Launches.py
+++ Launches.py
@@ -1,36 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Key a (path):
-#
-#  1 - angle
-#  2 - turn radius
-#  3 - motor id
 
-# Key b (speed):
-#
-#  1 - cm/sec
-#  2 - deg/sec
-#  3 - rot/sec
 
-# Key c (target):
-#
-#  1 - drive centimeters
-#  2 - target centimeters
-#  3 - target degrees
-#  4 - seconds
-
-#   ["MoveType"        ,  a  ,  b  ,  c  ],
-#
-# LaunchName = [
-#   ["Drive"           ,  1  ,  1  ,  1  ],
-#   ["DriveUltrasonic" ,  1  ,  1  ,  2  ],
-#   ["Turn"            ,  2  ,  2  ,  3  ],
-#   ["LineFollow"      ,        1        ],
-#   ["MotorOn"         ,  3  ,  3        ],
-#   ["MotorOff"        ,  3              ],
-#   ["Wait"            ,              4  ],
-# ]
 
 # Run #1- Bench (M04) and Slide (M03)
 East = [
@@ -68,43 +40,7 @@ NorthEast = [
 # NOT UPDATED YET
 # Run #3- Treadmill (M11), Row machine (M12), Weight machine (M13), Cell phone (M10)
 NorthWest = [
-    #Starts turning the motors to lock the towers in place
-    ["MotorOn"         ,  0  , -4          ],
-    ["Wait"            , 0.80              ],
-    ["MotorOff"        ,  0                ],
-    ["MotorOn"         ,  1  ,  4          ],
-    ["Wait"            , 0.17              ],
-    ["MotorOff"        ,  1                ],
-    #Turns towrds traffic jam
-    ["Turn"            , 11  ,  30  ,  90  ],
-    #Drives until it has wedged under the traffic jam
-    ["DriveUltrasonic" ,  90 ,  20  ,  120 ],
-    #Fully lifts up traffic jam
-    ["Turn"            , 10  , -30  ,   0  ],
-    #Goes forward and turns towrds tan towers
-    ["Drive"           ,  0  ,  20  ,  10  ],
-    ["Turn"            ,  0  ,  30  ,  90  ],
-    ["DriveUltrasonic" ,  90 ,  20  ,  149 ],
-    ["Turn"            ,  0.9, -30  ,  0   ],
-    ["DriveUltrasonic" ,  0  ,  20  ,  50  ],
-    #Drops of tan towers
-    ["MotorOn"         ,  0  ,  4          ],
-    ["Wait"            , 0.80              ],
-    ["MotorOff"        ,  0                ],
-    #Backs up and turns towrds red towers
-    ["DriveUltrasonic" ,  0  , -20  ,  15  ],
-    ["Turn"            ,  0  ,  30  ,  90  ],
-    ["Drive"           , 90  , -20  , -30  ],
-    #Drops of red towers
-    ["MotorOn"         ,  1  , -4          ],
-    ["Wait"            , 0.17              ],
-    ["MotorOff"        ,  1                ],
-    ["Wait"            ,  1                ],
-    #Comes back home
-    ["Drive"           ,  90 , -20  , -50  ],
-    #Closes the door to fit in base
-    ["MotorOn"         ,  1  ,  0.01       ],
-    ["Drive"           ,  90 , -20  , -65  ],
+
 ]
 
 #Run #4- Tire Flip (M09), Robot dance (M07)
